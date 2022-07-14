4857e350a75bb1d702803a7f93ebb3871b633dde
PinYi Lee(李品毅) <PinYi_Lee@T-01010549ML1.local>
2022-07-14 22:36:02 +0800

    fix swagger

--- test/main_test.go
+++ test/main_test.go
@@ -6,10 +6,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/jarcoal/httpmock"
 	"github.com/pinyi-lee/core.account.totp.git/internal/app/router"
 	"github.com/pinyi-lee/core.account.totp.git/internal/pkg/config"
-	"github.com/pinyi-lee/core.account.totp.git/internal/pkg/http/client"
 	"github.com/pinyi-lee/core.account.totp.git/internal/pkg/logger"
 	"github.com/pinyi-lee/core.account.totp.git/internal/pkg/mongo"
 	"github.com/pinyi-lee/core.account.totp.git/test/container"
@@ -28,8 +26,6 @@ func TestMain(m *testing.M) {
 	Setup()
 	defer Close()
 
-	httpmock.ActivateNonDefault(client.Get().GetClient())
-
 	r := m.Run()
 	os.Exit(r)
 }
@@ -38,10 +34,6 @@ func Setup() {
 	var err error
 
 	if err = config.Setup(); err != nil {
-		log.Fatal(err)
-	}
-
-	if err = client.Setup(); err != nil {
 		log.Fatal(err)
 	}
 
